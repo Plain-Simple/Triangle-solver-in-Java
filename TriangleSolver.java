ba8751176661cec0e6efe9559f205e5d0f97d447
Derik K <derikk@users.noreply.github.com>
2014-10-17 14:32:24 -0400

    Added angle and triangle type detection

--- TriangleSolver.java
+++ TriangleSolver.java
@@ -10,49 +10,75 @@ import java.util.Scanner;
 
 public class TriangleSolver {
 
-	public static void main(String[] args) {
-		double[] triangle = new double[6];
-		Graphics(triangle);
-		triangle = Input(triangle);
-	}
-	
-	static void Graphics(double triangle[]) {
+  public static void main(String[] args) {
+    double[] triangle = new double[6];
+    Graphics();
+    triangle = Input(triangle);
+    Println("The triangle is " + GetAngleType(triangle) + " and " + GetTriangleType(triangle) + ".");
+  }
+  
+  static void Graphics() {
 
-		Println("Plain + Simple Triangle Solver Java edition");
-		Println("                       B");
-		Println("                       .");
-		Println("                      /|");
-		Println("                     / |");
-		Println("                    /  |");
-		Println("                   /   |");
-		Println("                  /    |");
-		Println("                 /     |");
-		Println("         Side C /      | Side A");
-		Println("               /       |");
-		Println("              /        |");
-		Println("             /         |");
-		Println("            /          |");
-		Println("           /           |");
-		Println("          /____________|");
-		Println("        A     Side B    C\n");
-	}
-	static void Println(String s) {
-		System.out.println(s);
-	}
-	static void Print(String s) {
-		System.out.print(s);
-	}
-	static double[] Input(double[] triangle) {
-		Scanner input = new Scanner(System.in);
-		for (int i = 0; i < 3; i++) {
-			Print("Enter side " + (char)(i+'A') + " (0 if unknown): ");
-			triangle[i] = input.nextInt();
-		}
-		for (int i = 0; i < 3; i++) {
-			Print("Enter angle " + (char)(i+'a') + " (0 if unknown): ");
-			triangle[2 * i] = input.nextInt();
-		}
-		input.close();
-		return triangle;
-	}
+    Println("Plain + Simple Triangle Solver Java edition");
+    Println("                       a");
+    Println("                       .");
+    Println("                      /|");
+    Println("                     / |");
+    Println("                    /  |");
+    Println("                   /   |");
+    Println("                  /    |");
+    Println("                 /     |");
+    Println("         Side A /      | Side B");
+    Println("               /       |");
+    Println("              /        |");
+    Println("             /         |");
+    Println("            /          |");
+    Println("           /           |");
+    Println("          /____________|");
+    Println("        c     Side C    b\n");
+  }
+  
+  static void Println(String s) {
+    System.out.println(s);
+  }
+  static void Print(String s) {
+    System.out.print(s);
+  }
+  
+  static double[] Input(double[] triangle) {
+    Scanner input = new Scanner(System.in);
+    /* Sides are odd */
+    for (int i = 0; i < 3; i++) {
+      Print("Enter side " + (char)(i+'A') + " (0 if unknown): ");
+      triangle[2 * i + 1] = input.nextInt();
+    }
+    /* Angles are even */
+    for (int i = 0; i < 3; i++) {
+      Print("Enter angle " + (char)(i+'a') + " (0 if unknown): ");
+      triangle[2 * i] = input.nextInt();
+    }
+    
+    input.close();
+    return triangle;
+  }
+  static String GetTriangleType(double triangle[]) {
+    if ((triangle[0] == triangle [2]) || (triangle[0] == triangle[4]) || (triangle[2] == triangle[4])) {
+      if((triangle[0] == triangle [2]) && (triangle[0] == triangle[4]) && (triangle[2] == triangle[4]))
+        return "equilateral";
+      return "isosceles";
+    }
+  return "scalene";
+  }
+  static String GetAngleType(double triangle[]) {
+    for (int i = 0; i < 3; i++) {
+      if (triangle[2 * i] == 90) {
+        return "right";
+      }
+      if (triangle[2 * i] > 90) {
+        return "obtuse";
+      }
+    }
+    /* If no right or obtuse angles were found, it must be an acute triangle */
+    return "acute";
+  }
 }
